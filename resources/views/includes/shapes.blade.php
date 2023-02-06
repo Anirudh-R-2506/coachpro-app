4db7161b609201a9aa703c702e0e4f27c8fb688c
Anirudh-R-2506 <anirudhnfs01@gmail.com>
2023-02-06 22:55:27 +0530

    Bug fixes

---
+++ resources/views/includes/shapes.blade.php
@@ -0,0 +1,152 @@
+<style>
+  svg {
+    width: 100% !important;
+  }
+</style>
+<div class="fixed top-0 left-0 w-full h-full" style="background-color: transparent !important; z-index: -1 !important; box-shadow: none !important;">
+  <div class="" style="background-color: transparent !important;">
+    <div class="flex items-center w-full h-1/2" style="background-color: transparent !important;">
+      <span class="left-0 " data-aos="fade-zoom-in" data-aos-delay="400" style="transform: rotateX(-180deg); box-shadow: none !important; z-index: -1 !important; background-color: transparent !important; width: 50vw !important; height: 50vh !important;">
+          <svg
+            width="500"
+            height="470"
+            viewBox="0 0 495 470"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="55"
+              cy="442"
+              r="138"
+              stroke="black"
+              stroke-opacity="0.1"
+              stroke-width="50"
+              class="animate-pulse2"        
+            />
+            <circle
+              cx="446"
+              r="39"
+              stroke="black"
+              stroke-opacity="0.1"
+              stroke-width="20"
+              class="animate-pulse2"
+            />
+            <path
+              d="M245.406 137.609L233.985 94.9852L276.609 106.406L245.406 137.609Z"
+              stroke="black"
+              stroke-opacity="0.08"
+              stroke-width="12"
+              class="animate-pulse2"
+            />
+          </svg>
+        </span> 
+        <span class="right-0 " data-aos="fade-zoom-in" data-aos-delay="400" style="transform: rotateX(180deg);box-shadow: none !important; z-index: -1 !important; background-color: transparent !important; width: 50vw !important; height: 50vh !important;">
+          <svg
+            width="493"
+            height="470"
+            viewBox="0 0 493 470"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="462"
+              cy="5"
+              r="138"
+              stroke="black"
+              stroke-opacity="0.1"
+              stroke-width="50"
+              class="animate-pulse2"
+            />
+            <circle
+              cx="49"
+              cy="470"
+              r="39"
+              stroke="black"
+              stroke-opacity="0.1"
+              stroke-width="20"
+              class="animate-pulse2"
+            />
+            <path
+              d="M222.393 226.701L272.808 213.192L259.299 263.607L222.393 226.701Z"
+              stroke="black"
+              stroke-opacity="0.06"
+              stroke-width="13"
+              class="animate-pulse2"
+            />
+          </svg>
+        </span>       
+    </div>
+    <div class="flex items-center w-full h-1/2" style="background-color: transparent !important;">
+      <span data-aos="fade-zoom-in" data-aos-delay="400" class="right-0 " style="z-index: -1 !important; box-shadow: none !important; background-color: transparent !important; width: 50vw !important; height: 50vh !important;">
+        <svg
+          width="500"
+          height="470"
+          viewBox="0 0 495 470"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="55"
+            cy="442"
+            r="138"
+            stroke="black"
+            stroke-opacity="0.1"
+            stroke-width="50"
+            class="animate-pulse1"
+          />
+          <circle
+            cx="446"
+            r="39"
+            stroke="black"
+            stroke-opacity="0.1"
+            stroke-width="20"
+            class="animate-pulse1"
+          />
+          <path
+            d="M245.406 137.609L233.985 94.9852L276.609 106.406L245.406 137.609Z"
+            stroke="black"
+            stroke-opacity="0.08"
+            stroke-width="12"
+            class="animate-pulse1"
+          />
+        </svg>
+      </span>        
+        <span data-aos="fade-zoom-in" data-aos-delay="400" class="left-0 " style="z-index: -1 !important; box-shadow: none !important; background-color: transparent !important; width: 50vw !important; height: 50vh !important;">
+          <svg
+            width="493"
+            height="470"
+            viewBox="0 0 493 470"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="462"
+              cy="5"
+              r="138"
+              stroke="black"
+              stroke-opacity="0.1"
+              stroke-width="50"
+              class="animate-pulse1"
+            />
+            <circle
+              cx="49"
+              cy="470"
+              r="39"
+              stroke="black"
+              stroke-opacity="0.1"
+              stroke-width="20"
+              class="animate-pulse1"
+            />
+            <path
+              d="M222.393 226.701L272.808 213.192L259.299 263.607L222.393 226.701Z"
+              stroke="black"
+              stroke-opacity="0.06"
+              stroke-width="13"
+              class="animate-pulse1"
+            />
+          </svg>
+        </span>
+    </div>
+    </div>
+  </div>
+</div>
