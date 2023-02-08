f91010a6f2c947d046bc7622679b1c45110764b4
Anirudh-R-2506 <anirudhnfs01@gmail.com>
2023-02-08 20:02:37 +0530

    Bug fixes: GA tags

--- config/filament-google-analytics-realtime.php
+++ config/filament-google-analytics-realtime.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'view_id' => '',
+    'view_id' => '284429943',
     'key_file' => storage_path('app/analytics/service-account-credentials.json'),
     'interval' => '60s',
 ];
