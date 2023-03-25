217b9d219b248a043fba1b1296b52c9cfb55f3f6
Anirudh R <anirudhnfs01@gmail.com>
2023-03-26 03:00:46 +0530

    Bug fixes: Foreign key changes

--- database/migrations/2023_03_25_204655_foreign_key_changes.php
+++ database/migrations/2023_03_25_204655_foreign_key_changes.php
@@ -14,26 +14,26 @@ return new class extends Migration
     public function up()
     {
         Schema::table('courses', function (Blueprint $table) {
-            /* $table->dropForeign(['institute_id']); */
+            $table->dropForeign(['institute_id']);
             $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade')->change();
         });
 
         Schema::table('faculties', function (Blueprint $table) {
-            /* $table->dropForeign(['institute_id']); */
+            $table->dropForeign(['institute_id']);
             $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade')->change();
         });
 
         Schema::table('institutes', function (Blueprint $table) {
-            /* $table->dropForeign(['city_id']);
-            $table->dropForeign(['locality_id']); */
+            $table->dropForeign(['city_id']);
+            $table->dropForeign(['locality_id']);
             $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
             $table->foreign('locality_id')->references('id')->on('localities')->onDelete('cascade');
         });
 
         Schema::table('leads', function (Blueprint $table) {
-            /* $table->dropForeign(['course_id']);
+            $table->dropForeign(['course_id']);
             $table->dropForeign(['institute_id']);
-            $table->dropForeign(['user_id']); */
+            $table->dropForeign(['user_id']);
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->uuid('institute_id');
             $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade');
@@ -41,9 +41,9 @@ return new class extends Migration
         });
 
         Schema::table('bookings', function (Blueprint $table) {
-            /* $table->dropForeign(['course_id']);
+            $table->dropForeign(['course_id']);
             $table->dropForeign(['institute_id']);
-            $table->dropForeign(['user_id']); */
+            $table->dropForeign(['user_id']);
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->uuid('institute_id');
             $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade');
@@ -51,14 +51,13 @@ return new class extends Migration
         });
 
         Schema::table('faqs', function (Blueprint $table) {
-            /* $table->dropForeign(['course_id']); */
+            $table->dropForeign(['course_id']);
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
         });
 
         Schema::table('users', function (Blueprint $table) {
-            /* $table->dropForeign(['city']);
+            $table->dropForeign(['city']);
             $table->dropForeign(['locality']);
-            $table->dropForeign(['institute_id']); */
             $table->foreign('city')->references('id')->on('cities');
             $table->foreign('locality')->references('id')->on('localities');
             $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade')->nullable();
